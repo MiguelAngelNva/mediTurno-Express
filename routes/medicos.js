0e24cced67d7b92c77d8c1a4362b77224fd92c80
Miguel <miguelangelnava_4@hotmail.com>
2025-06-23 20:16:36 -0500

    Actualizando el formulario para agregar y editar medicos con todos sus atributos

--- routes/medicos.js
+++ routes/medicos.js
@@ -15,35 +15,50 @@ router.get('/vista', async (req, res) => {
 
 // Vista: formulario para crear nuevo médico
 router.get('/nuevo', async (req, res) => {
-    try {
-        const especialidades = await Especialidad.findAll();
-        res.render('medicos/nuevo', { especialidades });
-    } catch (err) {
-        res.status(500).send('Error al cargar formulario');
-    }
+    const { Especialidad, Documento, Estado } = require('../models');
+    const especialidades = await Especialidad.findAll();
+    const documentos = await Documento.findAll();
+    const estados = await Estado.findAll();
+
+    res.render('medicos/nuevo', {
+        especialidades,
+        documentos,
+        estados
+    });
 });
 
 // crear médico desde formulario
 router.post('/crear', async (req, res) => {
-    try {
-        const { esp_ids, ...medicoData } = req.body;
-        const medico = await Medico.create(medicoData);
-        if (esp_ids) await medico.addEspecialidades(esp_ids);
-        res.redirect('/medicos/vista');
-    } catch (err) {
-        res.status(500).send('Error al crear médico');
+    const { Medico } = require('../models');
+    const { esp_ids, ...medicoData } = req.body;
+
+    const medico = await Medico.create(medicoData);
+
+    if (esp_ids) {
+        // Si solo hay un ID, lo convertimos a array
+        const espArray = Array.isArray(esp_ids) ? esp_ids : [esp_ids];
+        await medico.addEspecialidades(espArray);
     }
+
+    res.redirect('/medicos/vista');
 });
 
 // Vista: formulario para editar un médico
 router.get('/:id/editar', async (req, res) => {
-    try {
-        const medico = await Medico.findByPk(req.params.id, { include: Especialidad });
-        const especialidades = await Especialidad.findAll();
-        res.render('medicos/editar', { medico, especialidades });
-    } catch (err) {
-        res.status(500).send('Error al cargar formulario de edición');
-    }
+    const { Medico, Especialidad, Documento, Estado } = require('../models');
+    const medico = await Medico.findByPk(req.params.id, {
+        include: Especialidad
+    });
+    const especialidades = await Especialidad.findAll();
+    const documentos = await Documento.findAll();
+    const estados = await Estado.findAll();
+
+    res.render('medicos/editar', {
+        medico,
+        especialidades,
+        documentos,
+        estados
+    });
 });
 
 // actualizar médico desde formulario
